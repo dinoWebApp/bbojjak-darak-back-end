2a5f2ddfb42f862d224037e5bdadc72d832461c8
nomadWebDeveloper <kgjsmart1559@gmail.com>
2025-07-20 22:39:45 +0900

    first commit

---
+++ prisma/migrations/20250717134410_create_account/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_email_key" ON "Account"("email");
